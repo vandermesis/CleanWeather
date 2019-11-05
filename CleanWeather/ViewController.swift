31fa80985a8b655aa32ba87ebbd82dbdb2740c18
Marek Skrzelowski <mskrzelowski@vandermesis.com>
2019-11-05 21:34:17 +0100

    Initial Commit

---
+++ CleanWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanWeather
+//
+//  Created by Marek Skrzelowski on 05/11/2019.
+//  Copyright © 2019 vandermesis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
