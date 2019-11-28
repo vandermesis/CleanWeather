564888664a2407b0505cb03e923f4179c9aa831b
Marek Skrzelowski <mskrzelowski@vandermesis.com>
2019-11-28 15:41:27 +0100

    Add Extensions unit tests

--- CleanWeather/Extensions/ErrorExtension.swift
+++ CleanWeather/Extensions/ErrorExtension.swift
@@ -20,7 +20,10 @@ extension Error {
             return R.string.localizable.decodingError()
         } else if let appError = self as? AppError {
             return appError.message
+        } else if self is UnitTestError {
+            return R.string.localizable.unitTestError()
+        } else {
+            return R.string.localizable.unknownError()
         }
-        return R.string.localizable.unknownError()
     }
 }
