3a2956b57f713437e9a3240ae6cd76c945205820
Marek Skrzelowski <mskrzelowski@vandermesis.com>
2019-12-06 14:37:48 +0100

    Fix FavouriteCitiesInteractor optional binding solution

--- CleanWeather/Scenes/FavouriteCities/FavourtieCitiesInteractor.swift
+++ CleanWeather/Scenes/FavouriteCities/FavourtieCitiesInteractor.swift
@@ -15,7 +15,7 @@ protocol FavouriteCitiesInteractor {
 
 final class FavouriteCitiesInteractorImpl {
 
-    private var city = [City]()
+    private var allCities = [City]()
     private var favouriteCities = [FavouriteCity]()
 
     private let presenter: FavouriteCitiesPresenter
@@ -40,9 +40,9 @@ extension FavouriteCitiesInteractorImpl: FavouriteCitiesInteractor {
             self?.presenter.toggleSpinner(false)
             switch result {
             case .success(let city):
-                self?.city = city
-                guard let favourites = self?.favouriteCities else { return }
-                self?.presenter.presentCities(city: city, favourites: favourites)
+                guard let self = self else { return }
+                self.allCities = city
+                self.presenter.presentCities(city: city, favourites: self.favouriteCities)
             case .failure(let error):
                 self?.presenter.presentError(error)
             }
@@ -50,10 +50,10 @@ extension FavouriteCitiesInteractorImpl: FavouriteCitiesInteractor {
     }
 
     func didSelectCity(name: String) {
-        guard let selectedCity = city.first(where: { $0.name == name }) else { return }
+        guard let selectedCity = allCities.first(where: { $0.name == name }) else { return }
         let favouriteCity = FavouriteCity(name: selectedCity.name, lat: selectedCity.latitude, lon: selectedCity.longitude, favourite: true)
         worker.toogleFavourite(for: favouriteCity)
         favouriteCities = worker.loadFavouriteCities()
-        presenter.presentCities(city: city, favourites: favouriteCities)
+        presenter.presentCities(city: allCities, favourites: favouriteCities)
     }
 }
