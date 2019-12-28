d9d1235a1a81e74f73aa7153f81dc4a6191f04f7
Marek Skrzelowski <mskrzelowski@vandermesis.com>
2019-12-28 16:53:17 +0100

    Clean CityListWorker from my bullshit

--- CleanWeather/Scenes/CityList/CityListWorker.swift
+++ CleanWeather/Scenes/CityList/CityListWorker.swift
@@ -20,12 +20,6 @@ final class CityListWorkerImpl {
     private let networking: WeatherNetworking
     private let repository: FavouriteCityRepository
 
-    private var citiesWeather = [CityWeather]()
-    private var errors = [Error]()
-    private var citiesCounter = 0
-    private var responseCounter = 0
-    private var errorCounter = 0
-
     init(networking: WeatherNetworking, repository: FavouriteCityRepository) {
         self.networking = networking
         self.repository = repository
@@ -39,51 +33,24 @@ extension CityListWorkerImpl: CityListWorker {
     }
 
     func fetchCitiesWeather(cities: [City], completion: FetchWeatherCompletion?) {
-        responseCounter = 0
-        errorCounter = 0
-        citiesCounter = cities.count
         cities.forEach { city in
             let coordinates = Coordinates(lat: city.latitude, lon: city.longitude)
             networking.fetchCurrentWeatherForCity(coordinates: coordinates.stringValue) { response in
                 self.handleCityWeatherResponse(city: city, response: response, completion: completion)
             }
         }
-        citiesWeather = []
     }
 }
 
 private extension CityListWorkerImpl {
-    
+
+    //TODO: Apply sortCity method after proper build of fetchCitiesWeather method
     private func sortCity(city: [CityWeather]) -> [CityWeather] {
         return city.sorted(by: { $0.city < $1.city })
     }
 
     private func handleCityWeatherResponse(city: City, response: Result<CityListAPIResponse, Error>, completion: FetchWeatherCompletion?) {
-        switch response {
-        case .success(let apiResponse):
-            if let cityTemperature = apiResponse.currently.temperature {
-                let cityWeather = CityWeather(id: city.id,
-                                              city: city.name,
-                                              latitude: city.latitude,
-                                              longitude: city.longitude,
-                                              temperature: cityTemperature,
-                                              icon: apiResponse.currently.icon ?? "")
-                citiesWeather.append(cityWeather)
-                responseCounter += 1
-            } else {
-                errors.append(MissingAPIData())
-                errorCounter += 1
-            }
-        case .failure(let error):
-            errors.append(error)
-            errorCounter += 1
-        }
-        if responseCounter + errorCounter == citiesCounter {
-            completion?(.success(citiesWeather))
-        }
-        if errorCounter > 0 {
-            guard let error = errors.last else { return }
-            completion?(.failure(error))
-        }
+        var responses = [Result<CityListAPIResponse, Error>]()
+        responses.append(response)
     }
 }
