5466cd0c822b3c5421d54c31a6f5cfdf54c61c28
Marek Skrzelowski <mskrzelowski@vandermesis.com>
2019-12-28 09:47:06 +0100

    Move urls to APIURL constants file

--- CleanWeather/Networking/WeatherNetworking.swift
+++ CleanWeather/Networking/WeatherNetworking.swift
@@ -21,31 +21,29 @@ protocol WeatherNetworking {
 }
 
 final class WeatherNetworkingImpl: BaseNetworking, WeatherNetworking {
-
-    let darkskyUrlPath = "https://api.darksky.net/forecast/598abc19ca5e8af71b5f7110f11d1ccf/"
     
     func fetchCurrentWeatherForCity(coordinates: String, completion: FetchWeatherResponseCompletion?) {
         let urlParams = "?exclude=minutely,hourly,daily,flags,alerts&lang=en&units=si"
-        let httpRequest = Request(url: darkskyUrlPath + coordinates + urlParams, method: .get, completion: completion)
+        let httpRequest = Request(url: APIURL.darkSky + coordinates + urlParams, method: .get, completion: completion)
         client.perform(request: httpRequest)
     }
     
     func fetchForecastWeatherForCity(coordinates: String, completion: FetchForecastResponseCompletion?) {
         let urlParams = "?exclude=currently,minutely,daily,flags,alerts&lang=en&units=si"
-        let httpRequest = Request(url: darkskyUrlPath + coordinates + urlParams, method: .get, completion: completion)
+        let httpRequest = Request(url: APIURL.darkSky + coordinates + urlParams, method: .get, completion: completion)
         client.perform(request: httpRequest)
     }
 
     func fetchHistoricalWeatherForCity(coordinates: String, date: Double, completion: FetchHistoricalResponseCompletion?) {
         let urlParams = "?exclude=minutely,hourly,daily,flags,alerts&lang=en&units=si"
         let urlDate = ",\(Int(date))"
-        let httpRequest = Request(url: darkskyUrlPath + coordinates + urlDate + urlParams, method: .get, completion: completion)
+        let httpRequest = Request(url: APIURL.darkSky + coordinates + urlDate + urlParams, method: .get, completion: completion)
         client.perform(request: httpRequest)
     }
     
     func fetchCities(completion: FetchCitiesResponseCompletion?) {
-        let url = "https://public.opendatasoft.com/api/records/1.0/search/?dataset=worldcitiespop&lang=pl&sort=population&facet=country&refine.country=pl"
-        let httpRequest = Request(url: url, method: .get, completion: completion)
+        let urlParams = "?dataset=worldcitiespop&lang=pl&sort=population&facet=country&refine.country=pl&rows=100"
+        let httpRequest = Request(url: APIURL.openDataSoft + urlParams, method: .get, completion: completion)
         client.perform(request: httpRequest)
     }
 }
