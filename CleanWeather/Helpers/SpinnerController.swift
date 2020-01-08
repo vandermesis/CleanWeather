1407967ce0909776b9a8d5059e461961478fa528
Marek Skrzelowski <mskrzelowski@vandermesis.com>
2020-01-08 14:44:25 +0100

    Modify SpinnerController with custom animation
    Changes:
    	Import Lottie library
    	Change default spinner with lottie animation

--- CleanWeather/Helpers/SpinnerController.swift
+++ CleanWeather/Helpers/SpinnerController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import Lottie
 
 final class SpinnerController: UIViewController {
+
+    @IBOutlet private weak var lottieView: AnimationView?
 
     init() {
         super.init(nibName: nil, bundle: nil)
@@ -18,4 +21,24 @@ final class SpinnerController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+    override func viewDidLoad() {
+        setupLottieView(animation: .sodaLoader)
+    }
+}
+
+private extension SpinnerController {
+
+    private func setupLottieView(animation: String) {
+        lottieView?.animation = Animation.named(animation)
+        lottieView?.loopMode = .loop
+        lottieView?.animationSpeed = 2.5
+        lottieView?.play()
+    }
+}
+
+private extension String {
+
+    static let colorDots = "1221-loader-animation"
+    static let sodaLoader = "94-soda-loader"
 }
